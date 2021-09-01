9538192bb8759ab21bf382f50a76b3e44a0adeb5
Adrian Forest <Adrian.Forest@cedarcc.com>
2021-09-01 18:18:29 +1000

    add comment to document interface method

--- ToyRobotLib/IRobotController.cs
+++ ToyRobotLib/IRobotController.cs
@@ -2,6 +2,12 @@
 {
   public interface IRobotController
   {
+    /// <summary>
+    /// Parses string input for valid command, and modifies the robot's position and/or facing if 
+    /// the command is valid.
+    /// </summary>
+    /// <param name="command">The command to execute</param>
+    /// <returns>The robot's current position and facing after the input is parsed.</returns>
     string CommandInput(string command);
   }
 }
