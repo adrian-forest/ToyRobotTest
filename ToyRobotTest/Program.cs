f8cee530376e8edfcf9cfaafd65e6d311325a89f
Adrian Forest <Adrian.Forest@cedarcc.com>
2021-09-01 18:26:18 +1000

    add basic loop to pass console input to robot library and display response

--- ToyRobotTest/Program.cs
+++ ToyRobotTest/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using ToyRobotLib;
 
 namespace ToyRobotTest
 {
   class Program
   {
-    static void Main(string[] args)
+
+    static void Main()
     {
-      Console.WriteLine("Hello World!");
+      // Initialise robot controller
+      IRobotController controller = new RobotController();
+
+      Console.WriteLine(controller.CommandInput("REPORT"));
+      Console.WriteLine("Please enter a command or hit 'x' to exit:");
+
+      string input = string.Empty;
+      while (input.ToLowerInvariant() != "x")
+      {
+        if (!string.IsNullOrWhiteSpace(input))
+        {
+          Console.WriteLine(controller.CommandInput(input));
+        }
+        input = Console.ReadLine();
+      }
     }
   }
 }
