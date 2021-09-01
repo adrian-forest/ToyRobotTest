ee393f97b320ba868d56e987c68b78f1259f6ed1
Adrian Forest <Adrian.Forest@cedarcc.com>
2021-09-01 16:02:44 +1000

    add stub for robot controller implementation

---
+++ ToyRobotLib/RobotController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ToyRobotLib
+{
+  public class RobotController : IRobotController
+  {
+    public RobotController()
+    {
+
+    }
+    public string CommandInput(string command)
+    {
+      return string.Empty;
+    }
+  }
+}
